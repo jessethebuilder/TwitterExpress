37e1556b360249f04bd965d926ef0fd52f545d93
Jesse Farmer <jessethebuilder@yahoo.com>
2015-02-28 04:46:39 -0800

    gem updates

--- app/helpers/twitter_bootstrap_helper.rb
+++ app/helpers/twitter_bootstrap_helper.rb
@@ -1,21 +1,28 @@
 module  TwitterBootstrapHelper
 
   IMAGE_TYPES = %w|png jpg jpeg gif|
+  #
+  # def express_carousel(objects, use_indicators: true, use_labels: true, carousel_id: 'express_carousel',
+  #                      image_version: :carousel, data_interval: 5000)
+  #   #expects object to have an #images association, in which each image responds to image_url, like carrier_wave.
+  #   #Each image object should also respond to #description
+  #   images = []
+  #   objects = [objects] unless objects.is_a?(ActiveRecord::Relation)
+  #   objects.each do |o|
+  #     o.images.each do |i|
+  #       images << i
+  #     end
+  #   end
+  #   render :partial => 'twitter_bootstrap_partials/carousel',
+  #          :locals => {:images => images, :use_indicators => use_indicators, :use_labels => use_labels,
+  #                      :carousel_id => carousel_id, :image_version => image_version, :data_interval => data_interval}
+  # end
 
-  def express_carousel(objects, use_indicators: true, use_labels: true, carousel_id: 'express_carousel',
-                       image_version: :carousel, data_interval: 5000)
-    #expects object to have an #images association, in which each image responds to image_url, like carrier_wave.
-    #Each image object should also respond to #description
-    images = []
-    objects = [objects] unless objects.is_a?(ActiveRecord::Relation)
-    objects.each do |o|
-      o.images.each do |i|
-        images << i
-      end
-    end
+  def express_carousel(image_urls, labels: [], use_indicators: true, element_id: 'express_carousel', slide_delay: 5000)
     render :partial => 'twitter_bootstrap_partials/carousel',
-           :locals => {:images => images, :use_indicators => use_indicators, :use_labels => use_labels,
-                       :carousel_id => carousel_id, :image_version => image_version, :data_interval => data_interval}
+          :locals => {:images => image_urls, :use_indicators => use_indicators, :labels => labels,
+                      :element_id => element_id,
+                      :delay => slide_delay}
   end
 
   def express_modal(title, footer_content: nil, destroy_on_close: false, element_id: 'express_modal', &content)
